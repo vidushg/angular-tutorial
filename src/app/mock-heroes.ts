3a84027d0a7469f5c26c22d54e4b298664a4c934
Vidush Goswami <goswamiv@grinnell.edu>
2021-03-05 20:09:46 -0600

    updating home+added mock data

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'NycBoi'},
+    {id: 33, name: 'NoyceBoi'},
+    {id: 22, name: 'ICBoi'},
+    {id: 28, name: 'SpeedyBoi'}
+]
